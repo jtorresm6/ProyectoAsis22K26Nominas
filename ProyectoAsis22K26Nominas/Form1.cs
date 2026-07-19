91edf061c902d77a53cb005cf55b8c78117c1ddf
231091 - JOSE JAVIER TORRES MARTINEZ <jtorresm6@miumg.edu.gt>
2026-07-18 20:57:31 -0600

    Pasando Form1 a MaterialSkin

--- ProyectoAsis22K26Nominas/Form1.cs
+++ ProyectoAsis22K26Nominas/Form1.cs
@@ -1,20 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using MaterialSkin.Controls;
+using System;
 
 namespace ProyectoAsis22K26Nominas
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MaterialForm
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void materialFlatButton1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }

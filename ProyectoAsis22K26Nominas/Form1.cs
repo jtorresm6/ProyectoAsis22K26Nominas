c006eea5442acf296afccd3caf2c2ce4416def2e
231091 - JOSE JAVIER TORRES MARTINEZ <jtorresm6@miumg.edu.gt>
2026-07-20 19:36:05 -0600

    Cambio agregacion de boton

--- ProyectoAsis22K26Nominas/Form1.cs
+++ ProyectoAsis22K26Nominas/Form1.cs
@@ -14,5 +14,20 @@ namespace ProyectoAsis22K26Nominas
         {
 
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void materialLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //////////////////////hollllllllllllaaaaaaaaaaaaaaaaaaaaaa
+        }
     }
 }

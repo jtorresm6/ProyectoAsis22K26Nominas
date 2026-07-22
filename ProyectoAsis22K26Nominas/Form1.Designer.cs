1fa460a28ad6416ca3b77e4e6b992257f6976f05
Roger Yankhel de Jesús Herrera Alcántara 0901-23-2429 <rherreraa9@miumg.edu.gt>
2026-07-22 08:28:30 -0600

    Prueba

--- ProyectoAsis22K26Nominas/Form1.Designer.cs
+++ ProyectoAsis22K26Nominas/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(193, 281);
+            this.button1.Location = new System.Drawing.Point(212, 193);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(343, 60);
             this.button1.TabIndex = 0;

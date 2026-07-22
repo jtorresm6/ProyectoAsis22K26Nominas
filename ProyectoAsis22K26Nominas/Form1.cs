1a97905eaae2127afc9aa39317335c826555122d
231091 - JOSE JAVIER TORRES MARTINEZ <jtorresm6@miumg.edu.gt>
2026-07-22 08:19:26 -0600

    Cambio de Forms

--- ProyectoAsis22K26Nominas/Form1.cs
+++ ProyectoAsis22K26Nominas/Form1.cs
@@ -1,9 +1,10 @@
 ﻿using MaterialSkin.Controls;
 using System;
+using System.Windows.Forms;
 
 namespace ProyectoAsis22K26Nominas
 {
-    public partial class Form1 : MaterialForm
+    public partial class Form1 : Form
     {
         public Form1()
         {

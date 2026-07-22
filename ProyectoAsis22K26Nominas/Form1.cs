9ac05dbe38d7fe435c4b976c5b4dc946fec19afa
231091 - JOSE JAVIER TORRES MARTINEZ <jtorresm6@miumg.edu.gt>
2026-07-22 08:59:56 -0600

    ejemplo

--- ProyectoAsis22K26Nominas/Form1.cs
+++ ProyectoAsis22K26Nominas/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows.Forms;
 
 namespace ProyectoAsis22K26Nominas
 {
@@ -6,6 +7,7 @@ namespace ProyectoAsis22K26Nominas
     {
         public Form1()
         {
+            ///hola mundo
             InitializeComponent();
         }
     }

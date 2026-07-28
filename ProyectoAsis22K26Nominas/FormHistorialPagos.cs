88ea7d6035262c1447b7e5cbc8e23a529c81ed0c
231091 - JOSE JAVIER TORRES MARTINEZ <jtorresm6@miumg.edu.gt>
2026-07-27 20:06:36 -0600

    Trabajo de Diego chen sin uso de windows dorms

--- ProyectoAsis22K26Nominas/FormHistorialPagos.cs
+++ ProyectoAsis22K26Nominas/FormHistorialPagos.cs
@@ -1,25 +1,224 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace ProyectoAsis22K26Nominas
 {
     public partial class FormHistorialPagos : Form
     {
+        private Label lblDesde;
+        private Label lblHasta;
+        private DateTimePicker dtpDesde;
+        private DateTimePicker dtpHasta;
+        private DataGridView dgvHistorialPagos;
+        private Button btnFiltrar;
+
+        private string conexion = "server=localhost;database=db_sistema_nominas;uid=root;pwd=;";
+
         public FormHistorialPagos()
         {
             InitializeComponent();
+            InicializarControles();
+            CargarHistorialPagos();
         }
 
-        private void FormHistorialPagos_Load(object sender, EventArgs e)
+        private void InicializarControles()
         {
+            this.BackColor = Color.FromArgb(27, 24, 37);
 
+            lblDesde = new Label();
+            lblHasta = new Label();
+            dtpDesde = new DateTimePicker();
+            dtpHasta = new DateTimePicker();
+            dgvHistorialPagos = new DataGridView();
+            btnFiltrar = new Button();
+
+
+            lblDesde.Text = "Desde:";
+            lblDesde.Location = new Point(20, 20);
+            lblDesde.AutoSize = true;
+            lblDesde.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            lblDesde.ForeColor = Color.WhiteSmoke;
+
+
+            dtpDesde.Location = new Point(80, 15);
+            dtpDesde.Size = new Size(140, 25);
+            dtpDesde.Format = DateTimePickerFormat.Short;
+
+
+            lblHasta.Text = "Hasta:";
+            lblHasta.Location = new Point(245, 20);
+            lblHasta.AutoSize = true;
+            lblHasta.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            lblHasta.ForeColor = Color.WhiteSmoke;
+
+
+            dtpHasta.Location = new Point(305, 15);
+            dtpHasta.Size = new Size(140, 25);
+            dtpHasta.Format = DateTimePickerFormat.Short;
+
+
+            btnFiltrar.Name = "btnFiltrar";
+            btnFiltrar.Text = "Filtrar";
+            btnFiltrar.Location = new Point(470, 14);
+            btnFiltrar.Size = new Size(120, 35);
+            btnFiltrar.BackColor = Color.FromArgb(235, 22, 86);
+            btnFiltrar.ForeColor = Color.White;
+            btnFiltrar.FlatStyle = FlatStyle.Flat;
+            btnFiltrar.FlatAppearance.BorderSize = 0;
+            btnFiltrar.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            btnFiltrar.Cursor = Cursors.Hand;
+            btnFiltrar.Click += BtnFiltrar_Click;
+
+
+            dgvHistorialPagos.Location = new Point(20, 70);
+            dgvHistorialPagos.Size = new Size(760, 360);
+            dgvHistorialPagos.AllowUserToAddRows = false;
+            dgvHistorialPagos.AllowUserToDeleteRows = false;
+            dgvHistorialPagos.ReadOnly = true;
+            dgvHistorialPagos.MultiSelect = false;
+            dgvHistorialPagos.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvHistorialPagos.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            dgvHistorialPagos.BackgroundColor = Color.FromArgb(32, 30, 43);
+            dgvHistorialPagos.BorderStyle = BorderStyle.None;
+            dgvHistorialPagos.GridColor = Color.FromArgb(55, 55, 70);
+            dgvHistorialPagos.RowHeadersVisible = false;
+            dgvHistorialPagos.EnableHeadersVisualStyles = false;
+
+
+            dgvHistorialPagos.ColumnHeadersHeight = 38;
+            dgvHistorialPagos.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dgvHistorialPagos.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(235, 22, 86);
+            dgvHistorialPagos.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            dgvHistorialPagos.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            dgvHistorialPagos.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+
+            dgvHistorialPagos.DefaultCellStyle.BackColor = Color.FromArgb(40, 37, 54);
+            dgvHistorialPagos.DefaultCellStyle.ForeColor = Color.WhiteSmoke;
+            dgvHistorialPagos.DefaultCellStyle.SelectionBackColor = Color.FromArgb(235, 22, 86);
+            dgvHistorialPagos.DefaultCellStyle.SelectionForeColor = Color.White;
+            dgvHistorialPagos.DefaultCellStyle.Font = new Font("Segoe UI", 10);
+
+            dgvHistorialPagos.RowsDefaultCellStyle.BackColor = Color.FromArgb(40, 37, 54);
+            dgvHistorialPagos.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(48, 45, 62);
+
+
+            Controls.Add(lblDesde);
+            Controls.Add(dtpDesde);
+            Controls.Add(lblHasta);
+            Controls.Add(dtpHasta);
+            Controls.Add(btnFiltrar);
+            Controls.Add(dgvHistorialPagos);
+        }
+
+
+        private void CargarHistorialPagos()
+        {
+            try
+            {
+                using (MySqlConnection cn = new MySqlConnection(conexion))
+                {
+                    cn.Open();
+
+                    string query = @"
+                    SELECT
+                        p.cmp_fecha_pago AS Fecha,
+                        CONCAT(e.cmp_nombre,' ',e.cmp_apellido) AS Empleado,
+                        p.cmp_forma_pago AS TipoPago,
+                        p.cmp_numero_recibo AS Transferencia,
+                        p.cmp_monto_pagado AS Monto
+                    FROM tbl_pagos p
+                    INNER JOIN tbl_detalle_planilla d
+                    ON p.cmp_id_detalle_planilla = d.cmp_id_detalle_planilla
+                    INNER JOIN tbl_empleados e
+                    ON d.cmp_id_empleado = e.cmp_id_empleado";
+
+                    MySqlCommand cmd = new MySqlCommand(query, cn);
+
+                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+
+                    DataTable dt = new DataTable();
+
+                    da.Fill(dt);
+
+                    dgvHistorialPagos.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    ex.Message,
+                    "Error de conexión",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void BtnFiltrar_Click(object sender, EventArgs e)
+        {
+            if (dtpDesde.Value.Date > dtpHasta.Value.Date)
+            {
+                MessageBox.Show(
+                    "La fecha Desde no puede ser mayor que Hasta.",
+                    "Fechas inválidas",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+
+                dtpDesde.Focus();
+                return;
+            }
+
+
+            try
+            {
+                using (MySqlConnection cn = new MySqlConnection(conexion))
+                {
+                    cn.Open();
+
+                    string query = @"
+                    SELECT
+                        p.cmp_fecha_pago AS Fecha,
+                        CONCAT(e.cmp_nombre,' ',e.cmp_apellido) AS Empleado,
+                        p.cmp_forma_pago AS TipoPago,
+                        p.cmp_numero_recibo AS Transferencia,
+                        p.cmp_monto_pagado AS Monto
+                    FROM tbl_pagos p
+                    INNER JOIN tbl_detalle_planilla d
+                    ON p.cmp_id_detalle_planilla = d.cmp_id_detalle_planilla
+                    INNER JOIN tbl_empleados e
+                    ON d.cmp_id_empleado = e.cmp_id_empleado
+                    WHERE p.cmp_fecha_pago BETWEEN @desde AND @hasta";
+
+
+                    MySqlCommand cmd = new MySqlCommand(query, cn);
+
+                    cmd.Parameters.AddWithValue("@desde", dtpDesde.Value.Date);
+                    cmd.Parameters.AddWithValue("@hasta", dtpHasta.Value.Date);
+
+
+                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+
+                    DataTable dt = new DataTable();
+
+                    da.Fill(dt);
+
+                    dgvHistorialPagos.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    ex.Message,
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
         }
     }
 }
--- ProyectoAsis22K26Nominas/FormHistorialVacaciones.cs
+++ ProyectoAsis22K26Nominas/FormHistorialVacaciones.cs
@@ -1,20 +1,178 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace ProyectoAsis22K26Nominas
 {
     public partial class FormHistorialVacaciones : Form
     {
+        private DataGridView dgvHistorialVacaciones;
+        private Button btnGenerarConstancia;
+
+        private string conexion = "server=localhost;database=db_sistema_nominas;uid=root;pwd=;";
+
+
         public FormHistorialVacaciones()
         {
             InitializeComponent();
+            InicializarControles();
+            CargarHistorialVacaciones();
+        }
+
+
+        private void InicializarControles()
+        {
+            this.BackColor = Color.FromArgb(27, 24, 37);
+
+            dgvHistorialVacaciones = new DataGridView();
+            btnGenerarConstancia = new Button();
+
+
+            dgvHistorialVacaciones.Location = new Point(20, 20);
+            dgvHistorialVacaciones.Size = new Size(760, 380);
+            dgvHistorialVacaciones.AllowUserToAddRows = false;
+            dgvHistorialVacaciones.AllowUserToDeleteRows = false;
+            dgvHistorialVacaciones.ReadOnly = true;
+            dgvHistorialVacaciones.MultiSelect = false;
+            dgvHistorialVacaciones.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvHistorialVacaciones.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvHistorialVacaciones.BorderStyle = BorderStyle.None;
+            dgvHistorialVacaciones.BackgroundColor = Color.FromArgb(32, 30, 43);
+            dgvHistorialVacaciones.GridColor = Color.FromArgb(55, 55, 70);
+            dgvHistorialVacaciones.RowHeadersVisible = false;
+            dgvHistorialVacaciones.EnableHeadersVisualStyles = false;
+
+
+            dgvHistorialVacaciones.ColumnHeadersHeight = 35;
+            dgvHistorialVacaciones.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(235, 22, 86);
+            dgvHistorialVacaciones.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            dgvHistorialVacaciones.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            dgvHistorialVacaciones.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+
+            dgvHistorialVacaciones.DefaultCellStyle.BackColor = Color.FromArgb(40, 37, 54);
+            dgvHistorialVacaciones.DefaultCellStyle.ForeColor = Color.WhiteSmoke;
+            dgvHistorialVacaciones.DefaultCellStyle.SelectionBackColor = Color.FromArgb(235, 22, 86);
+            dgvHistorialVacaciones.DefaultCellStyle.SelectionForeColor = Color.White;
+            dgvHistorialVacaciones.DefaultCellStyle.Font = new Font("Segoe UI", 10);
+
+            dgvHistorialVacaciones.RowsDefaultCellStyle.BackColor = Color.FromArgb(40, 37, 54);
+            dgvHistorialVacaciones.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(48, 45, 62);
+
+
+
+            btnGenerarConstancia.Name = "btnGenerarConstancia";
+            btnGenerarConstancia.Text = "Generar Constancia";
+            btnGenerarConstancia.Location = new Point(570, 420);
+            btnGenerarConstancia.Size = new Size(210, 40);
+            btnGenerarConstancia.BackColor = Color.FromArgb(235, 22, 86);
+            btnGenerarConstancia.ForeColor = Color.White;
+            btnGenerarConstancia.FlatStyle = FlatStyle.Flat;
+            btnGenerarConstancia.FlatAppearance.BorderSize = 0;
+            btnGenerarConstancia.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            btnGenerarConstancia.Cursor = Cursors.Hand;
+
+            btnGenerarConstancia.Click += BtnGenerarConstancia_Click;
+
+
+            Controls.Add(dgvHistorialVacaciones);
+            Controls.Add(btnGenerarConstancia);
+        }
+
+
+
+        private void CargarHistorialVacaciones()
+        {
+            try
+            {
+                using (MySqlConnection cn = new MySqlConnection(conexion))
+                {
+                    cn.Open();
+
+
+                    string query = @"
+                    SELECT
+                        v.cmp_fecha_inicio AS 'Fecha Inicio',
+                        v.cmp_fecha_fin AS 'Fecha Fin',
+                        v.cmp_cantidad_dias AS 'Días',
+                        v.cmp_motivo AS 'Motivo',
+                        v.cmp_estado AS 'Estado'
+                    FROM tbl_vacaciones v
+                    ORDER BY v.cmp_fecha_inicio DESC";
+
+
+                    MySqlDataAdapter da = new MySqlDataAdapter(query, cn);
+
+                    DataTable dt = new DataTable();
+
+                    da.Fill(dt);
+
+                    dgvHistorialVacaciones.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    ex.Message,
+                    "Error de conexión",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        private void BtnGenerarConstancia_Click(object sender, EventArgs e)
+        {
+            if (dgvHistorialVacaciones.SelectedRows.Count == 0)
+            {
+                MessageBox.Show(
+                    "Seleccione un registro de vacaciones.",
+                    "Aviso",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+
+                return;
+            }
+
+
+            DataGridViewRow fila = dgvHistorialVacaciones.SelectedRows[0];
+
+
+            string contenido =
+                "CONSTANCIA DE VACACIONES\n" +
+                "===========================\n\n" +
+                "Fecha Inicio: " + fila.Cells["Fecha Inicio"].Value.ToString() + "\n" +
+                "Fecha Fin: " + fila.Cells["Fecha Fin"].Value.ToString() + "\n" +
+                "Cantidad de días: " + fila.Cells["Días"].Value.ToString() + "\n" +
+                "Motivo: " + fila.Cells["Motivo"].Value.ToString() + "\n" +
+                "Estado: " + fila.Cells["Estado"].Value.ToString() + "\n\n" +
+                "Sistema de Nóminas";
+
+
+            SaveFileDialog guardar = new SaveFileDialog();
+
+            guardar.Filter = "Archivo de texto (*.txt)|*.txt";
+            guardar.FileName = "Constancia_Vacaciones.txt";
+
+
+            if (guardar.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.File.WriteAllText(
+                    guardar.FileName,
+                    contenido
+                );
+
+
+                MessageBox.Show(
+                    "Constancia descargada correctamente.",
+                    "Constancia",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
         }
     }
 }

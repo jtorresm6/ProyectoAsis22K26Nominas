885807af575f2f6b2349241ba441b379e16acfe8
231091 - JOSE JAVIER TORRES MARTINEZ <jtorresm6@miumg.edu.gt>
2026-07-29 17:42:07 -0600

    Cambio configuracion base de datos

--- ProyectoAsis22K26Nominas/ConexionBD.cs
+++ ProyectoAsis22K26Nominas/ConexionBD.cs
@@ -11,7 +11,7 @@ namespace ProyectoAsis22K26Nominas
     public class ConexionBD
     {
 
-        private static string cadenaConexion = "Server=192.168.1.115;Port=3306;Database=BD_ProyectoNominas;Uid=usuprueba;Pwd=1234;";
+        private static string cadenaConexion = "Server=172.20.10.4;Port=3306;Database=BD_ProyectoNominas;Uid=usuprueba;Pwd=1234;";
 
         // Método estático accesible directamente como ConexionBD.ObtenerConexion()
         public static MySqlConnection ObtenerConexion()

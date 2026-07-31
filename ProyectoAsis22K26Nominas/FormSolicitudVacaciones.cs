740696c55b2654ca19ba6e02eb127e4d7e766079
Julio Rosales <jrosalesm14@miumg.edu.gt>
2026-07-31 10:24:25 -0600

    Inicio de Desarrollo de Fomularios: Solicitud de Vacaciones y Control de Saldos.

--- ProyectoAsis22K26Nominas/FormSolicitudVacaciones.cs
+++ ProyectoAsis22K26Nominas/FormSolicitudVacaciones.cs
@@ -8,6 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//Parte trabajada por: Julio Roberto Rosales Mejía - Carné: 0901-23-1426
+//Curso:Análisis de Sistemas II
+//Fecha de creación: 27/07/2026
+//Fecha de última modificación: 29/07/2026
+
 namespace ProyectoAsis22K26Nominas
 {
     public partial class FormSolicitudVacaciones : Form

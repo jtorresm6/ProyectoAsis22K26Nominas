7995dc0c4bcb46c7defee31eda32def927cbed0e
231091 - JOSE JAVIER TORRES MARTINEZ <jtorresm6@miumg.edu.gt>
2026-07-31 21:05:47 -0600

    Guardar cambios locales en SesionUsuario

---
+++ ProyectoAsis22K26Nominas/SesionUsuario.cs
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoAsis22K26Nominas
+{
+    internal static class SesionUsuario
+    {
+        public static int IdUsuario { get; set; }
+        public static string Usuario { get; set; }
+        public static int IdRol { get; set; }
+        public static string Rol { get; set; }
+        public static string NombreCompleto { get; set; }
+    }
+}
+
+>>>>>>> c538409 (Actualización completa de permisos, clases para los usuarios, permisos y bitacora)

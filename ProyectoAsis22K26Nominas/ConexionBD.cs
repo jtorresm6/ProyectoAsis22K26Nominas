9d1a90319f532100dac340877eb50cf583670444
Julio Rosales <jrosalesm14@miumg.edu.gt>
2026-08-03 22:47:03 -0600

    Modificación de Archivo "ConexionBD.cs" (Ignorar)

--- ProyectoAsis22K26Nominas/ConexionBD.cs
+++ ProyectoAsis22K26Nominas/ConexionBD.cs
@@ -1,10 +1,5 @@
 ﻿using MySql.Data.MySqlClient;
 
-//Parte trabajada por: Jose Javier Torres Martinez - Carné: 0901-23-1091
-//Curso:Análisis de Sistemas II
-//Fecha de creación: 23-07-2026
-//Fecha de última modificación: 27-07-2026
-
 namespace ProyectoAsis22K26Nominas
 {
     public class ConexionBD

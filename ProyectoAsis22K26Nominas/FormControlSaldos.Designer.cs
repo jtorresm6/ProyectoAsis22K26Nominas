b16dcfee17cdd977f3679b348db5ad2ec69c599c
231091 - JOSE JAVIER TORRES MARTINEZ <jtorresm6@miumg.edu.gt>
2026-08-04 19:13:21 -0600

    Agregacion de Formulario de control de saldos y solicitud de saldos, parte de Julio

--- ProyectoAsis22K26Nominas/FormControlSaldos.Designer.cs
+++ ProyectoAsis22K26Nominas/FormControlSaldos.Designer.cs
@@ -28,21 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Gbo_Busqueda_Trabajador = new System.Windows.Forms.GroupBox();
+            this.Cbo_Buscar_Empleado = new System.Windows.Forms.ComboBox();
+            this.Lbl_Buscar_Empleado = new System.Windows.Forms.Label();
+            this.Gbo_Saldo_Vacaciones = new System.Windows.Forms.GroupBox();
+            this.Dgv_Saldo_Vacaciones = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Dtp_Dia_Vacaciones = new System.Windows.Forms.DateTimePicker();
+            this.Btn_Refrescar = new System.Windows.Forms.Button();
+            this.Gbo_Busqueda_Trabajador.SuspendLayout();
+            this.Gbo_Saldo_Vacaciones.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_Saldo_Vacaciones)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(270, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(335, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CONTROL DE SALDO DE VACACIONES";
+            // 
+            // Gbo_Busqueda_Trabajador
+            // 
+            this.Gbo_Busqueda_Trabajador.Controls.Add(this.Cbo_Buscar_Empleado);
+            this.Gbo_Busqueda_Trabajador.Controls.Add(this.Lbl_Buscar_Empleado);
+            this.Gbo_Busqueda_Trabajador.Location = new System.Drawing.Point(13, 60);
+            this.Gbo_Busqueda_Trabajador.Name = "Gbo_Busqueda_Trabajador";
+            this.Gbo_Busqueda_Trabajador.Size = new System.Drawing.Size(840, 90);
+            this.Gbo_Busqueda_Trabajador.TabIndex = 1;
+            this.Gbo_Busqueda_Trabajador.TabStop = false;
+            this.Gbo_Busqueda_Trabajador.Text = "Búsqueda de Trabajador";
+            // 
+            // Cbo_Buscar_Empleado
+            // 
+            this.Cbo_Buscar_Empleado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Cbo_Buscar_Empleado.FormattingEnabled = true;
+            this.Cbo_Buscar_Empleado.Location = new System.Drawing.Point(105, 38);
+            this.Cbo_Buscar_Empleado.Name = "Cbo_Buscar_Empleado";
+            this.Cbo_Buscar_Empleado.Size = new System.Drawing.Size(500, 24);
+            this.Cbo_Buscar_Empleado.TabIndex = 1;
+            // 
+            // Lbl_Buscar_Empleado
+            // 
+            this.Lbl_Buscar_Empleado.AutoSize = true;
+            this.Lbl_Buscar_Empleado.Location = new System.Drawing.Point(28, 41);
+            this.Lbl_Buscar_Empleado.Name = "Lbl_Buscar_Empleado";
+            this.Lbl_Buscar_Empleado.Size = new System.Drawing.Size(52, 16);
+            this.Lbl_Buscar_Empleado.TabIndex = 0;
+            this.Lbl_Buscar_Empleado.Text = "Buscar:";
+            // 
+            // Gbo_Saldo_Vacaciones
+            // 
+            this.Gbo_Saldo_Vacaciones.Controls.Add(this.Dgv_Saldo_Vacaciones);
+            this.Gbo_Saldo_Vacaciones.Location = new System.Drawing.Point(13, 214);
+            this.Gbo_Saldo_Vacaciones.Name = "Gbo_Saldo_Vacaciones";
+            this.Gbo_Saldo_Vacaciones.Size = new System.Drawing.Size(840, 314);
+            this.Gbo_Saldo_Vacaciones.TabIndex = 2;
+            this.Gbo_Saldo_Vacaciones.TabStop = false;
+            this.Gbo_Saldo_Vacaciones.Text = "Saldo de Vacaciones";
+            // 
+            // Dgv_Saldo_Vacaciones
+            // 
+            this.Dgv_Saldo_Vacaciones.AllowUserToAddRows = false;
+            this.Dgv_Saldo_Vacaciones.AllowUserToDeleteRows = false;
+            this.Dgv_Saldo_Vacaciones.AllowUserToResizeRows = false;
+            this.Dgv_Saldo_Vacaciones.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.Dgv_Saldo_Vacaciones.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.Dgv_Saldo_Vacaciones.BackgroundColor = System.Drawing.Color.White;
+            this.Dgv_Saldo_Vacaciones.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Dgv_Saldo_Vacaciones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv_Saldo_Vacaciones.Location = new System.Drawing.Point(15, 36);
+            this.Dgv_Saldo_Vacaciones.MultiSelect = false;
+            this.Dgv_Saldo_Vacaciones.Name = "Dgv_Saldo_Vacaciones";
+            this.Dgv_Saldo_Vacaciones.ReadOnly = true;
+            this.Dgv_Saldo_Vacaciones.RowHeadersVisible = false;
+            this.Dgv_Saldo_Vacaciones.RowHeadersWidth = 51;
+            this.Dgv_Saldo_Vacaciones.RowTemplate.Height = 24;
+            this.Dgv_Saldo_Vacaciones.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.Dgv_Saldo_Vacaciones.Size = new System.Drawing.Size(800, 260);
+            this.Dgv_Saldo_Vacaciones.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Seleccione una Fecha: ";
+            // 
+            // Dtp_Dia_Vacaciones
+            // 
+            this.Dtp_Dia_Vacaciones.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.Dtp_Dia_Vacaciones.Location = new System.Drawing.Point(169, 169);
+            this.Dtp_Dia_Vacaciones.Name = "Dtp_Dia_Vacaciones";
+            this.Dtp_Dia_Vacaciones.Size = new System.Drawing.Size(200, 22);
+            this.Dtp_Dia_Vacaciones.TabIndex = 4;
+            // 
+            // Btn_Refrescar
+            // 
+            this.Btn_Refrescar.Location = new System.Drawing.Point(394, 160);
+            this.Btn_Refrescar.Name = "Btn_Refrescar";
+            this.Btn_Refrescar.Size = new System.Drawing.Size(120, 45);
+            this.Btn_Refrescar.TabIndex = 5;
+            this.Btn_Refrescar.Text = "Refrescar";
+            this.Btn_Refrescar.UseVisualStyleBackColor = true;
             // 
             // FormControlSaldos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(21)))), ((int)(((byte)(32)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(882, 576);
+            this.Controls.Add(this.Btn_Refrescar);
+            this.Controls.Add(this.Dtp_Dia_Vacaciones);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Gbo_Saldo_Vacaciones);
+            this.Controls.Add(this.Gbo_Busqueda_Trabajador);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "FormControlSaldos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormControlSaldos";
             this.Load += new System.EventHandler(this.FormControlSaldos_Load);
+            this.Gbo_Busqueda_Trabajador.ResumeLayout(false);
+            this.Gbo_Busqueda_Trabajador.PerformLayout();
+            this.Gbo_Saldo_Vacaciones.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_Saldo_Vacaciones)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox Gbo_Busqueda_Trabajador;
+        private System.Windows.Forms.Label Lbl_Buscar_Empleado;
+        private System.Windows.Forms.ComboBox Cbo_Buscar_Empleado;
+        private System.Windows.Forms.GroupBox Gbo_Saldo_Vacaciones;
+        private System.Windows.Forms.DataGridView Dgv_Saldo_Vacaciones;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker Dtp_Dia_Vacaciones;
+        private System.Windows.Forms.Button Btn_Refrescar;
     }
 }
--- ProyectoAsis22K26Nominas/FormSolicitudVacaciones.Designer.cs
+++ ProyectoAsis22K26Nominas/FormSolicitudVacaciones.Designer.cs
@@ -28,21 +28,314 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Lbl_Titulo = new System.Windows.Forms.Label();
+            this.Gbo_Empleado = new System.Windows.Forms.GroupBox();
+            this.Lbl_Dias_Disponibles = new System.Windows.Forms.Label();
+            this.Lbl_Texto_Dias = new System.Windows.Forms.Label();
+            this.Cbo_Empleado = new System.Windows.Forms.ComboBox();
+            this.Lbl_Empleado = new System.Windows.Forms.Label();
+            this.Gbo_Solicitud = new System.Windows.Forms.GroupBox();
+            this.Txt_Motivo = new System.Windows.Forms.TextBox();
+            this.Lbl_Motivo = new System.Windows.Forms.Label();
+            this.Txt_Dias_Solicitados = new System.Windows.Forms.TextBox();
+            this.Lbl_Dias_Solicitados = new System.Windows.Forms.Label();
+            this.Dtp_Fecha_Fin = new System.Windows.Forms.DateTimePicker();
+            this.Lbl_Fin = new System.Windows.Forms.Label();
+            this.Dtp_Fecha_Inicio = new System.Windows.Forms.DateTimePicker();
+            this.Lbl_Inicio = new System.Windows.Forms.Label();
+            this.Btn_Guardar = new System.Windows.Forms.Button();
+            this.Btn_Aprobar = new System.Windows.Forms.Button();
+            this.Btn_Rechazar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Dgv_Solicitudes = new System.Windows.Forms.DataGridView();
+            this.Btn_Refrescar = new System.Windows.Forms.Button();
+            this.Btn_Limpiar = new System.Windows.Forms.Button();
+            this.Gbo_Empleado.SuspendLayout();
+            this.Gbo_Solicitud.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_Solicitudes)).BeginInit();
             this.SuspendLayout();
+            // 
+            // Lbl_Titulo
+            // 
+            this.Lbl_Titulo.AutoSize = true;
+            this.Lbl_Titulo.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Titulo.Location = new System.Drawing.Point(301, 9);
+            this.Lbl_Titulo.Name = "Lbl_Titulo";
+            this.Lbl_Titulo.Size = new System.Drawing.Size(251, 25);
+            this.Lbl_Titulo.TabIndex = 0;
+            this.Lbl_Titulo.Text = "SOLICITUD DE VACACIONES";
+            // 
+            // Gbo_Empleado
+            // 
+            this.Gbo_Empleado.Controls.Add(this.Lbl_Dias_Disponibles);
+            this.Gbo_Empleado.Controls.Add(this.Lbl_Texto_Dias);
+            this.Gbo_Empleado.Controls.Add(this.Cbo_Empleado);
+            this.Gbo_Empleado.Controls.Add(this.Lbl_Empleado);
+            this.Gbo_Empleado.Location = new System.Drawing.Point(12, 48);
+            this.Gbo_Empleado.Name = "Gbo_Empleado";
+            this.Gbo_Empleado.Size = new System.Drawing.Size(700, 120);
+            this.Gbo_Empleado.TabIndex = 1;
+            this.Gbo_Empleado.TabStop = false;
+            this.Gbo_Empleado.Text = "Información del Empleado";
+            // 
+            // Lbl_Dias_Disponibles
+            // 
+            this.Lbl_Dias_Disponibles.AutoSize = true;
+            this.Lbl_Dias_Disponibles.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Dias_Disponibles.Location = new System.Drawing.Point(148, 81);
+            this.Lbl_Dias_Disponibles.Name = "Lbl_Dias_Disponibles";
+            this.Lbl_Dias_Disponibles.Size = new System.Drawing.Size(49, 16);
+            this.Lbl_Dias_Disponibles.TabIndex = 3;
+            this.Lbl_Dias_Disponibles.Text = "0 días";
+            // 
+            // Lbl_Texto_Dias
+            // 
+            this.Lbl_Texto_Dias.AutoSize = true;
+            this.Lbl_Texto_Dias.Location = new System.Drawing.Point(26, 81);
+            this.Lbl_Texto_Dias.Name = "Lbl_Texto_Dias";
+            this.Lbl_Texto_Dias.Size = new System.Drawing.Size(116, 16);
+            this.Lbl_Texto_Dias.TabIndex = 2;
+            this.Lbl_Texto_Dias.Text = "Días Disponibles: ";
+            // 
+            // Cbo_Empleado
+            // 
+            this.Cbo_Empleado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Cbo_Empleado.FormattingEnabled = true;
+            this.Cbo_Empleado.Location = new System.Drawing.Point(120, 32);
+            this.Cbo_Empleado.Name = "Cbo_Empleado";
+            this.Cbo_Empleado.Size = new System.Drawing.Size(350, 24);
+            this.Cbo_Empleado.TabIndex = 1;
+            // 
+            // Lbl_Empleado
+            // 
+            this.Lbl_Empleado.AutoSize = true;
+            this.Lbl_Empleado.Location = new System.Drawing.Point(26, 35);
+            this.Lbl_Empleado.Name = "Lbl_Empleado";
+            this.Lbl_Empleado.Size = new System.Drawing.Size(76, 16);
+            this.Lbl_Empleado.TabIndex = 0;
+            this.Lbl_Empleado.Text = "Empleado: ";
+            // 
+            // Gbo_Solicitud
+            // 
+            this.Gbo_Solicitud.Controls.Add(this.Txt_Motivo);
+            this.Gbo_Solicitud.Controls.Add(this.Lbl_Motivo);
+            this.Gbo_Solicitud.Controls.Add(this.Txt_Dias_Solicitados);
+            this.Gbo_Solicitud.Controls.Add(this.Lbl_Dias_Solicitados);
+            this.Gbo_Solicitud.Controls.Add(this.Dtp_Fecha_Fin);
+            this.Gbo_Solicitud.Controls.Add(this.Lbl_Fin);
+            this.Gbo_Solicitud.Controls.Add(this.Dtp_Fecha_Inicio);
+            this.Gbo_Solicitud.Controls.Add(this.Lbl_Inicio);
+            this.Gbo_Solicitud.Location = new System.Drawing.Point(14, 196);
+            this.Gbo_Solicitud.Name = "Gbo_Solicitud";
+            this.Gbo_Solicitud.Size = new System.Drawing.Size(700, 285);
+            this.Gbo_Solicitud.TabIndex = 2;
+            this.Gbo_Solicitud.TabStop = false;
+            this.Gbo_Solicitud.Text = "Información Solicitud";
+            // 
+            // Txt_Motivo
+            // 
+            this.Txt_Motivo.Location = new System.Drawing.Point(38, 190);
+            this.Txt_Motivo.Multiline = true;
+            this.Txt_Motivo.Name = "Txt_Motivo";
+            this.Txt_Motivo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Txt_Motivo.Size = new System.Drawing.Size(500, 80);
+            this.Txt_Motivo.TabIndex = 7;
+            // 
+            // Lbl_Motivo
+            // 
+            this.Lbl_Motivo.AutoSize = true;
+            this.Lbl_Motivo.Location = new System.Drawing.Point(35, 171);
+            this.Lbl_Motivo.Name = "Lbl_Motivo";
+            this.Lbl_Motivo.Size = new System.Drawing.Size(50, 16);
+            this.Lbl_Motivo.TabIndex = 6;
+            this.Lbl_Motivo.Text = "Motivo:";
+            // 
+            // Txt_Dias_Solicitados
+            // 
+            this.Txt_Dias_Solicitados.Location = new System.Drawing.Point(149, 123);
+            this.Txt_Dias_Solicitados.Name = "Txt_Dias_Solicitados";
+            this.Txt_Dias_Solicitados.Size = new System.Drawing.Size(100, 22);
+            this.Txt_Dias_Solicitados.TabIndex = 5;
+            this.Txt_Dias_Solicitados.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Lbl_Dias_Solicitados
+            // 
+            this.Lbl_Dias_Solicitados.AutoSize = true;
+            this.Lbl_Dias_Solicitados.Location = new System.Drawing.Point(30, 129);
+            this.Lbl_Dias_Solicitados.Name = "Lbl_Dias_Solicitados";
+            this.Lbl_Dias_Solicitados.Size = new System.Drawing.Size(108, 16);
+            this.Lbl_Dias_Solicitados.TabIndex = 4;
+            this.Lbl_Dias_Solicitados.Text = "Días Solicitados:";
+            // 
+            // Dtp_Fecha_Fin
+            // 
+            this.Dtp_Fecha_Fin.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.Dtp_Fecha_Fin.Location = new System.Drawing.Point(149, 78);
+            this.Dtp_Fecha_Fin.Name = "Dtp_Fecha_Fin";
+            this.Dtp_Fecha_Fin.Size = new System.Drawing.Size(200, 22);
+            this.Dtp_Fecha_Fin.TabIndex = 3;
+            // 
+            // Lbl_Fin
+            // 
+            this.Lbl_Fin.AutoSize = true;
+            this.Lbl_Fin.Location = new System.Drawing.Point(30, 83);
+            this.Lbl_Fin.Name = "Lbl_Fin";
+            this.Lbl_Fin.Size = new System.Drawing.Size(72, 16);
+            this.Lbl_Fin.TabIndex = 2;
+            this.Lbl_Fin.Text = "Fecha Fin: ";
+            // 
+            // Dtp_Fecha_Inicio
+            // 
+            this.Dtp_Fecha_Inicio.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.Dtp_Fecha_Inicio.Location = new System.Drawing.Point(149, 37);
+            this.Dtp_Fecha_Inicio.Name = "Dtp_Fecha_Inicio";
+            this.Dtp_Fecha_Inicio.Size = new System.Drawing.Size(200, 22);
+            this.Dtp_Fecha_Inicio.TabIndex = 1;
+            // 
+            // Lbl_Inicio
+            // 
+            this.Lbl_Inicio.AutoSize = true;
+            this.Lbl_Inicio.Location = new System.Drawing.Point(30, 43);
+            this.Lbl_Inicio.Name = "Lbl_Inicio";
+            this.Lbl_Inicio.Size = new System.Drawing.Size(85, 16);
+            this.Lbl_Inicio.TabIndex = 0;
+            this.Lbl_Inicio.Text = "Fecha Inicio: ";
+            // 
+            // Btn_Guardar
+            // 
+            this.Btn_Guardar.Location = new System.Drawing.Point(16, 503);
+            this.Btn_Guardar.Name = "Btn_Guardar";
+            this.Btn_Guardar.Size = new System.Drawing.Size(150, 45);
+            this.Btn_Guardar.TabIndex = 3;
+            this.Btn_Guardar.Text = "Guardar Solicitud";
+            this.Btn_Guardar.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Aprobar
+            // 
+            this.Btn_Aprobar.Location = new System.Drawing.Point(189, 503);
+            this.Btn_Aprobar.Name = "Btn_Aprobar";
+            this.Btn_Aprobar.Size = new System.Drawing.Size(120, 45);
+            this.Btn_Aprobar.TabIndex = 4;
+            this.Btn_Aprobar.Text = "Aprobar";
+            this.Btn_Aprobar.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Rechazar
+            // 
+            this.Btn_Rechazar.Location = new System.Drawing.Point(338, 503);
+            this.Btn_Rechazar.Name = "Btn_Rechazar";
+            this.Btn_Rechazar.Size = new System.Drawing.Size(120, 45);
+            this.Btn_Rechazar.TabIndex = 5;
+            this.Btn_Rechazar.Text = "Rechazar";
+            this.Btn_Rechazar.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Dgv_Solicitudes);
+            this.groupBox1.Location = new System.Drawing.Point(19, 565);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(844, 318);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Solicitudes Registradas";
+            // 
+            // Dgv_Solicitudes
+            // 
+            this.Dgv_Solicitudes.AllowUserToAddRows = false;
+            this.Dgv_Solicitudes.AllowUserToDeleteRows = false;
+            this.Dgv_Solicitudes.AllowUserToResizeRows = false;
+            this.Dgv_Solicitudes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.Dgv_Solicitudes.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.Dgv_Solicitudes.BackgroundColor = System.Drawing.Color.White;
+            this.Dgv_Solicitudes.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Dgv_Solicitudes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv_Solicitudes.Location = new System.Drawing.Point(22, 30);
+            this.Dgv_Solicitudes.MultiSelect = false;
+            this.Dgv_Solicitudes.Name = "Dgv_Solicitudes";
+            this.Dgv_Solicitudes.ReadOnly = true;
+            this.Dgv_Solicitudes.RowHeadersVisible = false;
+            this.Dgv_Solicitudes.RowHeadersWidth = 51;
+            this.Dgv_Solicitudes.RowTemplate.Height = 24;
+            this.Dgv_Solicitudes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.Dgv_Solicitudes.Size = new System.Drawing.Size(800, 260);
+            this.Dgv_Solicitudes.TabIndex = 0;
+            // 
+            // Btn_Refrescar
+            // 
+            this.Btn_Refrescar.Location = new System.Drawing.Point(477, 503);
+            this.Btn_Refrescar.Name = "Btn_Refrescar";
+            this.Btn_Refrescar.Size = new System.Drawing.Size(150, 45);
+            this.Btn_Refrescar.TabIndex = 7;
+            this.Btn_Refrescar.Text = "Refrescar";
+            this.Btn_Refrescar.UseVisualStyleBackColor = true;
+            //this.Btn_Refrescar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Btn_Limpiar
+            // 
+            this.Btn_Limpiar.Location = new System.Drawing.Point(654, 503);
+            this.Btn_Limpiar.Name = "Btn_Limpiar";
+            this.Btn_Limpiar.Size = new System.Drawing.Size(145, 45);
+            this.Btn_Limpiar.TabIndex = 8;
+            this.Btn_Limpiar.Text = "Limpiar";
+            this.Btn_Limpiar.UseVisualStyleBackColor = true;
+            //this.Btn_Limpiar.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // FormSolicitudVacaciones
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(21)))), ((int)(((byte)(32)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(890, 953);
+            this.Controls.Add(this.Btn_Limpiar);
+            this.Controls.Add(this.Btn_Refrescar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Btn_Rechazar);
+            this.Controls.Add(this.Btn_Aprobar);
+            this.Controls.Add(this.Btn_Guardar);
+            this.Controls.Add(this.Gbo_Solicitud);
+            this.Controls.Add(this.Gbo_Empleado);
+            this.Controls.Add(this.Lbl_Titulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "FormSolicitudVacaciones";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormSolicitudVacaciones";
             this.Load += new System.EventHandler(this.FormSolicitudVacaciones_Load);
+            this.Gbo_Empleado.ResumeLayout(false);
+            this.Gbo_Empleado.PerformLayout();
+            this.Gbo_Solicitud.ResumeLayout(false);
+            this.Gbo_Solicitud.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_Solicitudes)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Lbl_Titulo;
+        private System.Windows.Forms.GroupBox Gbo_Empleado;
+        private System.Windows.Forms.Label Lbl_Empleado;
+        private System.Windows.Forms.ComboBox Cbo_Empleado;
+        private System.Windows.Forms.Label Lbl_Texto_Dias;
+        private System.Windows.Forms.Label Lbl_Dias_Disponibles;
+        private System.Windows.Forms.GroupBox Gbo_Solicitud;
+        private System.Windows.Forms.Label Lbl_Inicio;
+        private System.Windows.Forms.DateTimePicker Dtp_Fecha_Inicio;
+        private System.Windows.Forms.Label Lbl_Fin;
+        private System.Windows.Forms.DateTimePicker Dtp_Fecha_Fin;
+        private System.Windows.Forms.Label Lbl_Dias_Solicitados;
+        private System.Windows.Forms.TextBox Txt_Dias_Solicitados;
+        private System.Windows.Forms.Label Lbl_Motivo;
+        private System.Windows.Forms.TextBox Txt_Motivo;
+        private System.Windows.Forms.Button Btn_Guardar;
+        private System.Windows.Forms.Button Btn_Aprobar;
+        private System.Windows.Forms.Button Btn_Rechazar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView Dgv_Solicitudes;
+        private System.Windows.Forms.Button Btn_Refrescar;
+        private System.Windows.Forms.Button Btn_Limpiar;
     }
 }
